b16a10878c7e614dd79a8a857d58cadedf6f1fd8
kleiner-goliath <95882884+kleiner-goliath@users.noreply.github.com>
2022-01-03 21:06:04 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ let Beschleunigung_y = 0
 let Änderung = 0
 let Geschwindigkeit = 0
 let Leuchtet = 0
+let Messungen_y: number[] = []
 let Richtung = 1
 let Neigung = 5.7
 let Winkel = 17
@@ -17,9 +18,12 @@ let GradBogenmaß = Pi / 180
 let Offset = 1000 * Math.sin(Neigung * GradBogenmaß)
 let Faktor = Math.cos(Winkel * GradBogenmaß)
 let Verzögerung = 5
-let Minimum = 0.2
+let Minimum = 0.5
 let Index = 0
 let Länge = 24
+for (let index = 0; index < 10; index++) {
+    Messungen_y.push(0)
+}
 input.setAccelerometerRange(AcceleratorRange.FourG)
 let Streifen = neopixel.create(DigitalPin.P0, Länge, NeoPixelMode.RGB)
 Leuchtet = 0
@@ -54,7 +58,8 @@ basic.forever(function () {
 })
 basic.forever(function () {
     while (true) {
-        Beschleunigung_y = input.acceleration(Dimension.Y)
+        Messungen_y.unshift(input.acceleration(Dimension.Y))
+        Beschleunigung_y = Beschleunigung_y + (Messungen_y[0] / 10 - Messungen_y.pop() / 10)
         if (Beschleunigung_y > Offset) {
             BeschleunigungRotation = (Beschleunigung_y - Offset) / Faktor * millig
             Geschwindigkeit = Math.sqrt(BeschleunigungRotation / Radius) / _2Pi
